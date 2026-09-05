bf04516b30383ea3f0112e02b747086c58a24470
André <andreodesouza@outlook.com.br>
2026-09-05 13:54:45 -0300

    delete_modificações

--- core/urls.py
+++ core/urls.py
@@ -15,23 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from core.view import CadastroView
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # Novas rotas de cadastro
-    path('api/cadastro/usuario/', CadastroView.as_view(), name='cadastro_usuario'),
-    path('api/cadastro/empresa/', CadastroView.as_view(), name='cadastro_empresa'),
+    path("admin/", admin.site.urls),
 ]
-
-
-
